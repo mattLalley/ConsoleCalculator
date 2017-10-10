5b288a1932e0531b7c24097ae1b382c0eef4e234
Matthew Lalley <matt@l4digital.com>
2017-10-10 01:32:19 -0700

    initial commit

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleCalculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
